4df69f9dd7ed5ff9ef5c5484b00b022438b46ad4
ashik317 <mdarlikhon317@gmail.com>
2026-07-03 17:39:29 +0600

    Fix build.sh to run create_admin and seed_data

--- backend/build.sh
+++ backend/build.sh
@@ -3,11 +3,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# Create superuser if it doesn't already exist
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
-    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
-"
+python manage.py create_admin
+python manage.py seed_data
